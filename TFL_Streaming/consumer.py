546497e52fc00e2d1caed331f7ac3dc47437d360
lewtwelf <lewtwelf@gmail.com>
2025-11-26 16:12:41 +0000

    consumer file fixes

--- TFL_Streaming/consumer.py
+++ TFL_Streaming/consumer.py
@@ -1,7 +1,9 @@
 from pyspark.sql import SparkSession
+from kafka import KafkaConsumer
+import json
  
 spark = SparkSession.builder \
-    .appName("tfl_data_kafka_consumer") \
+    .appName("UK_TFL_DE011025_CONSUMER") \
     .getOrCreate()
  
 kafka_bootstrap = "ip-172-31-3-80.eu-west-2.compute.internal:9092"
@@ -18,6 +20,9 @@ df = spark.read \
  
 # Convert value to string
 parsed_df = df.selectExpr("CAST(value AS STRING) as json_value")
- 
+
+# Print to stout for Jeinkins logging
+parsed_df.show(truncate=False)
+
 # Save to HDFS
-parsed_df.write.mode("overwrite").json("hdfs:////tmp/DE011025/lew/kafka/test_lew")
+parsed_df.write.mode("overwrite").json("hdfs:////tmp/DE011025/uk/streaming/incoming_batches")
